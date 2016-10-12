e2884fd82c4af17a304ee728d5f45dd1b2c8df95
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-12 17:28:18 +0200

    indentation

--- json-file-reader.js
+++ json-file-reader.js
@@ -7,16 +7,16 @@ var fs = require('fs');
 function readJSON(filename, callback){
 	//read the JSON file if it doesn't throw an error
 	fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data){
-			if (err) {
-				throw err;
-			}
-			//read data
-			//save data in object you declare
-			//parse the read JSON data into JavaScript object (an array is also an object)
-			var readObject = JSON.parse(data);
-			//run the callback function in the app with the data stored in the var readObject
-			callback(readObject);
-			});
+		if (err) {
+			throw err;
+		}
+		//read data
+		//save data in object you declare
+		//parse the read JSON data into JavaScript object (an array is also an object)
+		var readObject = JSON.parse(data);
+		//run the callback function in the app with the data stored in the var readObject
+		callback(readObject);
+	});
 }
 
 //make the function readJSON accessible outside this file
